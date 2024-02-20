d5fdd77ea4ccf6a0678351f26f4787e96af66449
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-02-20 18:53:05 -0500

    Update test.py
    Wrote a program and added its contents into the file which was created on the Raspberry Pi

---
+++ test.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO
+import time
+
+# References GPIO pins by their physical pin numbers.
+GPIO.setmode(GPIO.BOARD)
+
+def get_direction_and_state(pin_num):
+    direction = "Out" if GPIO.gpio_function(pin_num) == GPIO.OUT else "In"
+    state = GPIO.input(pin_num)
+    return direction, state
+
+try:
+
+    array = [13,19,26]
+
+    # Configure pins, set to output
+    for num in array:
+        GPIO.setup(num, GPIO.OUT)
+        direction, state = get_direction_and_state(num)
+        print(f"Pin # {num} is set to {direction}, state: {state}\n")
+        time.sleep(1)
+
+    # Set to HIGH or LOW
+    for num in array:
+        direction, state = get_direction_and_state(num)
+        GPIO.output(num, GPIO.HIGH)
+        direction, state = get_direction_and_state(num)
+        print(f"Pin # {num} is set to {direction}, state: {state}\n")
+        time.sleep(1)
+
+        GPIO.output(num, GPIO.LOW)
+        direction, state = get_direction_and_state(num)
+        print(f"Pin # {num} is set to {direction}, state: {state}\n")
+        time.sleep(1)
+
+    # Set pins back to input
+    for num in array:     
+        GPIO.setup(num, GPIO.IN)
+        direction, state = get_direction_and_state(num)
+        print(f"Pin # {num} is set to {direction}, state: {state}\n")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+
+    GPIO.cleanup()
+# test 2
