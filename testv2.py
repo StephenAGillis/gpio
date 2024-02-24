1458aeb2b1e5b55438d4870739f27095254253d3
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-02-23 19:35:27 -0500

    Update testv2.py

--- testv2.py
+++ testv2.py
@@ -41,10 +41,20 @@ def set_led_color(x):
     GPIO.output(x, GPIO.LOW)
     time.sleep(1)
 
+def establish_rows(x)
+    for num in x:
+        GPIO.setup(x, GPIO.OUT)
+        GPIO.output(x, GPIO.HIGH)
+        time.sleep(1)
+
+def establish_cols(x)
+    for num in x:
+        GPIO.setup(x,GPIO.IN) 
+
 def set_pin_input(x):
     GPIO.setup(x, GPIO.IN)
     time.sleep(1)
-
+    
 def set_pin_output(x):
     GPIO.setup(x, GPIO.OUT)
     time.sleep(1)
@@ -54,11 +64,64 @@ rows = [18, 23, 24, 25] # top to bottom
 cols = [12, 16, 20, 21] # left to right
 
 try:
-    configure_pins(rows)
     configure_pins(led_pins)
-    set_high_and_low(led_pins)
+    establish_rows(rows)
+    establish_cols(cols)
+
+    while True:
+        for num in rows:
+            GPIO.output(num, GPIO.HIGH)
+            for num in cols:
+                GPIO.input(num, GPIO.IN)
+                if GPIO.input(12, GPIO.IN) == GPIO.HIGH:
+                    if GPIO.output(18, GPIO.HIGH):
+                        set_led_color(13) 
+                        
+    disable_pins(rows)
+    disable_pins(cols)
     disable_pins(led_pins)
     
 except KeyboardInterrupt:
 
     GPIO.cleanup()
+
+
+
+   12  16  20  21    (columns)
+18
+23
+24
+25
+(rows)
+
+setup
+
+loop through each row
+    set row to 5v
+    loop though each each column which is set to LOW
+        if column goes HIGH
+            perform action
+            set back to low
+            continue 
+    set row to 0v
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
