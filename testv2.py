9e6ded343f1b927811dadfeb73102910a446d930
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-02-24 00:18:59 -0500

    Update testv2.py

--- testv2.py
+++ testv2.py
@@ -4,119 +4,49 @@ import time
 # References GPIO pins by their physical pin numbers.
 GPIO.setmode(GPIO.BCM)
 
-def get_direction_and_state(x):
-    direction = "Out" if GPIO.gpio_function(x) == GPIO.OUT else "In"
-    state = GPIO.input(x)
-    print(f"Pin # {x} is set to {direction}, state: {state}")
-
-def configure_pins(x):
-    print("Entered configure_pins loop")
-    for num in x:
-        GPIO.setup(num, GPIO.OUT)
-        get_direction_and_state(num)
-        time.sleep(1)
-
-def disable_pins(x):
-    print("Entered disable_pins loop")
-    for num in x:
-        GPIO.setup(num, GPIO.IN)
-        get_direction_and_state(num)
-        time.sleep(1)
+def print_IO(x): # Print Digital Input/Output Pin(s)
+    state = GPIO.gpio_function(x)
+    print("pin state: ", x)
         
-def set_high_and_low(x):
-    print("Entered set_high_and_low loop")
-    for num in x:
-        GPIO.output(num, GPIO.HIGH)
-        time.sleep(1)
+def print_Volts(x):
+    voltage = "HIGH" if GPIO.input(x) else "Low"
+    print("Pin voltage: ", x)
 
-        get_direction_and_state(num)
-        
-        GPIO.output(num, GPIO.LOW)
-        time.sleep(1)
-
-def set_led_color(x):
+def set_high(x):
     GPIO.output(x, GPIO.HIGH)
-    time.sleep(1)
-    
+    state = GPIO.intput(x)
+    print(f"pin {x}'s state: {state} ")
+
+def set_low(x):
     GPIO.output(x, GPIO.LOW)
-    time.sleep(1)
+    state = GPIO.input(x)
+    print(f"pin {x}'s state: {state} ")
 
-def establish_rows(x)
-    for num in x:
-        GPIO.setup(x, GPIO.OUT)
-        GPIO.output(x, GPIO.HIGH)
-        time.sleep(1)
-
-def establish_cols(x)
-    for num in x:
-        GPIO.setup(x,GPIO.IN) 
-
-def set_pin_input(x):
+def set_input(x):
     GPIO.setup(x, GPIO.IN)
-    time.sleep(1)
-    
-def set_pin_output(x):
+    mode = GPIO.gpio_function(x)
+    print(f"pin {x}'s mode: {mode}") 
+
+def set_output(x):
     GPIO.setup(x, GPIO.OUT)
-    time.sleep(1)
-    
-led_pins = [13, 19, 26]
+    mode = GPIO.gpio_function(x)
+    print(f"pin {x}'s mode : {mode}")
+
+led = [13, 19, 26] # R G B settings
 rows = [18, 23, 24, 25] # top to bottom
 cols = [12, 16, 20, 21] # left to right
 
-try:
-    configure_pins(led_pins)
-    establish_rows(rows)
-    establish_cols(cols)
+# for i in range(len(led_pins)):
 
-    while True:
-        for num in rows:
-            GPIO.output(num, GPIO.HIGH)
-            for num in cols:
-                GPIO.input(num, GPIO.IN)
-                if GPIO.input(12, GPIO.IN) == GPIO.HIGH:
-                    if GPIO.output(18, GPIO.HIGH):
-                        set_led_color(13) 
-                        
-    disable_pins(rows)
-    disable_pins(cols)
-    disable_pins(led_pins)
+try:
+    set_output(led[1])
+    set_high(led[1])
+    time.sleep(2)
+    set_input(led[1])
     
 except KeyboardInterrupt:
 
     GPIO.cleanup()
-
-
-
-   12  16  20  21    (columns)
-18
-23
-24
-25
-(rows)
-
-setup
-
-loop through each row
-    set row to 5v
-    loop though each each column which is set to LOW
-        if column goes HIGH
-            perform action
-            set back to low
-            continue 
-    set row to 0v
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
