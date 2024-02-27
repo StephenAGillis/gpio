d0174e55bb01f6b42e0b0a6ac7e0bc3ed8f13d81
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-02-26 23:07:59 -0500

    Update testv2.py

--- testv2.py
+++ testv2.py
@@ -14,94 +14,74 @@ def print_Volts(x):
 
 def set_high(x):
     print("set_high")
-    for i in x:
-        GPIO.output(i, GPIO.HIGH)
-        state = GPIO.input(i)
-        print(f"pin {i}'s state: {state} ")
-        sleep()
+    GPIO.output(i, GPIO.HIGH)
+    state = GPIO.input(i)
+    print(f"pin {i}'s state: {state} ")
+    sleep()
 
 def set_low(x):
     print("set_low")
-    for i in x:
-        GPIO.output(i, GPIO.LOW)
-        state = GPIO.input(i)
-        print(f"pin {i}'s state: {state} ")
-        sleep()
+    GPIO.output(i, GPIO.LOW)
+    state = GPIO.input(i)
+    print(f"pin {i}'s state: {state} ")
+    sleep()
 
 def set_input(x):
     print("set_input")
-    for i in x:
-        GPIO.setup(i, GPIO.IN)
-        mode = GPIO.gpio_function(i)
-        print(f"pin {i}'s mode: IN") 
-        sleep()
+    GPIO.setup(i, GPIO.IN)
+    mode = GPIO.gpio_function(i)
+    print(f"pin {i}'s mode: IN") 
+    sleep()
 
 def set_output(x):
     print("set_output")
-    for i in x:
-        GPIO.setup(i, GPIO.OUT)
-        mode = GPIO.gpio_function(i)
-        print(f"pin {i}'s mode : OUT")
-        sleep()
+    GPIO.setup(i, GPIO.OUT)
+    mode = GPIO.gpio_function(i)
+    print(f"pin {i}'s mode : OUT")
+    sleep()
 
-def power_row(x):
-    pass
 
 def sleep():
     time.sleep(2)
 
-def set_state(x, pin):
-    if x == "HIGH":
-        GPIO.output(pin, GPIO.HIGH)
+#      12   16   20   21
+#  18  [1]  [2]  [3]  [A]
+#  23  [4]  [5]  [6]  [B]
+#  24  [7]  [8]  [9]  [C]
+#  25  [*]  [0]  [#]  [D]
+
+def find_index(x,y):
+    if x == 18 and y == 12:
+        print("button 1 is pushed")
+        GPIO.setup(13, GPIO.OUT)
+        GPIO.output(13, GPIO.HIGH)
         sleep()
-    if x == "LOW":
-        GPIO.output(pin, GPIO.LOW)
-        sleep()
-
-leds = [{"pin": 13, "mode": GPIO.OUT, "value": GPIO.HIGH},
-        {"pin": 19, "mode": GPIO.OUT, "value": GPIO.HIGH},
-        {"pin": 26, "mode": GPIO.OUT, "value": GPIO.HIGH}]    # R G B settings
-
-# Will be output, HIGH channels, top row to bottom row
-rows = [{"pin": 18, "mode": GPIO.OUT, "value": GPIO.HIGH},
-        {"pin": 23, "mode": GPIO.OUT, "value": GPIO.HIGH},
-        {"pin": 24, "mode": GPIO.OUT, "value": GPIO.HIGH},
-        {"pin": 25, "mode": GPIO.OUT, "value": GPIO.HIGH}]
-
-cols = [{"pin": 12, "mode": GPIO.IN, "value": GPIO.LOW},
-        {"pin": 16, "mode": GPIO.IN, "value": GPIO.LOW},
-        {"pin": 20, "mode": GPIO.IN, "value": GPIO.LOW},
-        {"pin": 21, "mode": GPIO.OUT, "value": GPIO.LOW}]    # left to right
+        GPIO.setup(13, GPIO.IN)
+    if x == 18 && y == 16:
+        pass
+    if x == 18 && y == 20:
+        pass
+    
 
 leds = [13,19,26]
 rows = [18,23,24,24]    # Top to bottom
 cols = [12,16,20,21]     #left to right
 
+
 try:
-    for led in leds:
-        pin = led
-        mode = GPIO.OUT
-        value = GPIO.HIGH
+    while True:
+        for i in rows:
+            GPIo.setup(i, GPIO.OUT)
+            GPIO.output(i, GPIO.HIGH)
         
-        GPIO.setup(pin, mode)
-        GPIO.setup(pin, value)
-        sleep()
-
-    for led in leds:
-        pin = led["pin"]
-        mode = GPIO.LOW
-        value = 
-        GPIO.output(pin, mode)
-        sleep()
-
-    for led in leds:
-        pin = led["pin"]
-        mode = 
-        value =
-
+            for j in cols:
+                GPIO.setup(j, GPIO.IN)
+                if GPIO.input(j) == GPIO.HIGH:
+                    find_index(i,j)
+            GPIO.output(i, GPIO.LOW)
+            GPIO.setup(i, GPIO.IN)
     
-    
-            
+
 except KeyboardInterrupt:
 
     GPIO.cleanup()
