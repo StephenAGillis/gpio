944c702beb5efe6a1659bbc0509919c758640248
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-03-05 21:58:14 -0500

    Update basicLED.py

--- basicLED.py
+++ basicLED.py
@@ -1,37 +1,41 @@
 import RPi.GPIO as GPIO
 import time
-from gpiozero import Button
+
 
 # Set up GPIO using BOARD numbering
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pin to use
-blue_button_pin = 16
-green_button_pin = 12
-yellow_button_pin = 21
-red_button_pin = 20
-red_LED_pin = 13
-blue_LED_pin = 26
-green_LED_pin = 19
-
-blue_button = Button(blue_button_pin)
-yellow_button = Button(yellow_button_pin)
+blue_button= 16
+green_button = 12
+yellow_button = 21
+red_button = 20
+red_LED = 13
+blue_LED = 26
+green_LED = 19
 
 # Set up the GPIO pin as an output
-GPIO.setup(red_button_pin, GPIO.OUT)
-GPIO.setup(red_LED_pin, GPIO.OUT)    
+GPIO.setup(green_button, GPIO.OUT)
+GPIO.setup(green_LED, GPIO.OUT)
+GPIO.setup(yellow_button, GPIO.OUT)
 
 try:
     
-    while(True):
+    while True:
 
-        if yellow_button.is_pressed:
-            GPIO.output(green_LED_pin, GPIO.LOW)
-            print(f"Pin {red_LED_pin} is turned OFF.")
+        GPIO.output(yellow_button, GPIO.HIGH)
+        GPIO.output(green_button, GPIO.HIGH)
+        time.sleep(0.1)
+        
+        if GPIO.input(yellow_button) == GPIO.LOW:
+            GPIO.output(green_LED, GPIO.LOW)
+            print(f"Pin {green_LED} is turned OFF.")
             break
-        if green_button.is_pressed:
-            GPIO.output(green_LED_pin, GPIO.HIGH)
-            print(f"Pin {green_LED_pin} is turned ON.")
+            
+        if GPIO.input(green_button) == GPIO.LOW:
+            GPIO.output(green_LED, GPIO.HIGH)
+            print(f"Pin {green_LED} is turned ON.")
+
     
 finally:
     # Clean up the GPIO settings
