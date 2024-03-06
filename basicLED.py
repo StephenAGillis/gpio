47a5c6b9b34ee33d5a48a9ad49d17b88c11bba2e
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-03-05 23:03:26 -0500

    Update basicLED.py

--- basicLED.py
+++ basicLED.py
@@ -15,6 +15,8 @@ red_LED = 13
 blue_LED = 26
 green_LED = 19
 
+interupt = 23
+
 # Set up the GPIO pin as an output
 GPIO.setup(green_button, GPIO.OUT)
 GPIO.setup(green_LED, GPIO.OUT)
@@ -23,25 +25,42 @@ GPIO.setup(yellow_button, GPIO.OUT)
 
 def led_color(x):
     GPIO.output(x, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(x, GPIO.LOW)
     print(f"Pin {x} is turned ON.")
+    
+    time.sleep(1)
+    
+    GPIO.output(x, GPIO.LOW)
+    print(f"Pin {x} is turned OFF.")
+
+def setup():
+    GPIO.output(interupt, GPIO.HIGH)
+    GPIO.output(green_button, GPIO.HIGH)
+    GPIO.output(blue_button, GPIO.HIGH)
+    GPIO.output(red_button, GPIO.HIGH)
+    GPIO.output(yellow_button, GPIO.HIGH)
 
 try:
     
     while True:
 
-        GPIO.output(yellow_button, GPIO.HIGH)
-        GPIO.output(green_button, GPIO.HIGH)
-        
-        if GPIO.input(yellow_button) == GPIO.LOW:
-            GPIO.output(green_LED, GPIO.LOW)
-            print(f"Pin {green_LED} is turned OFF.")
-            break
+        setup()
             
         if GPIO.input(green_button) == GPIO.LOW:
             led_color(green_LED)
 
+        if GPIO.input(blue_button) == GPIO.LOW:
+            led_color(blue_LED)
+            
+        if GPIO.input(red_button) == GPIO.LOW:
+            led_color(red_LED)
+            
+        if GPIO.input(blue_button) == GPIO.LOW:
+            led_color(blue_LED)
+
+        if GPIO.input(interupt) == GPIO.LOW:
+            GPIO.output(interupt, GPIO.LOW)
+            print(f"Pin {interupt} is turned OFF.")
+            break
     
 finally:
     # Clean up the GPIO settings
