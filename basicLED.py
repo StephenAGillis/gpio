6eb857302e2eb926de336a45f54a9c02149862af
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-03-05 21:40:05 -0500

    Update basicLED.py

--- basicLED.py
+++ basicLED.py
@@ -5,29 +5,32 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pin to use
-blue_button = 16
-green_button = 12
-yellow_button = 21
-red_button = 20
-red_LED = 13
-blue_LED = 26
-green_LED = 19
+blue_button_pin = 16
+green_button_pin = 12
+yellow_button_pin = 21
+red_button_pin = 20
+red_LED_pin = 13
+blue_LED_pin = 26
+green_LED_pin = 19
+
+green_button = Button(green_button_pin)
+yellow_button = Button(yellow_button_pin)
 
 # Set up the GPIO pin as an output
-GPIO.setup(red_button, GPIO.OUT)
-GPIO.setup(red_LED, GPIO.OUT)    
+GPIO.setup(red_button_pin, GPIO.OUT)
+GPIO.setup(red_LED_pin, GPIO.OUT)    
 
 try:
     
     while(True):
 
         if yellow_button.is_pressed:
-            GPIO.output(green_LED, GPIO.LOW)
-            print(f"Pin {red_LED} is turned OFF.")
+            GPIO.output(green_LED_pin, GPIO.LOW)
+            print(f"Pin {red_LED_pin} is turned OFF.")
             break
         if green_button.is_pressed:
-            GPIO.output(green_LED, GPIO.HIGH)
-            print(f"Pin {green_LED} is turned ON.")
+            GPIO.output(green_LED_pin, GPIO.HIGH)
+            print(f"Pin {green_LED_pin} is turned ON.")
     
 finally:
     # Clean up the GPIO settings
