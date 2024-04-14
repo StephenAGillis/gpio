76d81042a92b45d053a9dbd1dbb0ac95c3ed75c4
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-04-13 20:10:38 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,51 +1,39 @@
 import RPi.GPIO as GPIO
 import time
 
-# References GPIO pins by their physical pin numbers.
 GPIO.setmode(GPIO.BCM)
 
-def get_direction_and_state(pin_num):
-    direction = "Out" if GPIO.gpio_function(pin_num) == GPIO.OUT else "In"
-    state = GPIO.input(pin_num)
-    return direction, state
+# (digit#)port#    ::    (1)port5, (2)port13, (3)port19, (4)port26
+d1 = 5
+d2 = 13
+d3 = 19
+d4 = 26
 
-def configure_pins():
-    for num in array:
-        GPIO.setup(num, GPIO.OUT)
-        direction, state = get_direction_and_state(num)
-        print(f"Pin # {num} is set to {direction}, state: {state}\n")
-        time.sleep(1)
+def set_seq(a,b,c,d):
+    GPIO.output(a, GPIO.HIGH)
+    GPIO.output(a, GPIO.HIGH)
+    GPIO.output(a, GPIO.HIGH)
+    GPIO.output(a, GPIO.HIGH)
 
-def set_high_and_low():
-    for num in array:
-        direction, state = get_direction_and_state(num)
-        GPIO.output(num, GPIO.HIGH)
-        direction, state = get_direction_and_state(num)
-        print(f"Pin # {num} is set to {direction}, state: {state}\n")
-        time.sleep(1)
-        
-        GPIO.output(num, GPIO.LOW)
-        direction, state = get_direction_and_state(num)
-        print(f"Pin # {num} is set to {direction}, state: {state}\n")
-        time.sleep(1)
+def setup_ports(a,b,c,d)
+    GPIO.setup(a, GPIO.OUT)
+    GPIO.setup(b, GPIO.OUT)
+    GPIO.setup(c, GPIO.OUT)
+    GPIO.setup(d, GPIO.OUT)
 
-def set_reconfigure_pins():
-    for num in array:
-        GPIO.setup(num, GPIO.IN)
-        direction, state = get_direction_and_state(num)
-        print(f"Pin # {num} is set to {direction}, state: {state}\n")
-        time.sleep(1)
-
-def test_leds():
-    configure_pins()
-    set_high_and_low()
-    set_reconfigure_pins()
-
-array = [13,19,26]
+def set_low(a,b,c,d)
+    GPIO.output(a, GPIO.LOW)
+    GPIO.output(b, GPIO.LOW)
+    GPIO.output(c, GPIO.LOW)
+    GPIO.output(d, GPIO.LOW)
 
 try:
-    test_leds()
+    setup_ports(d1,d2,d3,d4)
+    time.sleep(1)
+    set_seq(d1,d2,d3,d4)
+    time.sleep(1)
+    set_low(d1,d2,d3,d4)
 
-except KeyboardInterrupt:
-
+finally:
     GPIO.cleanup()
+    
