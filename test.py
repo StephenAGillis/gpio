49f1c9f7692a76f7cb633e165e63c5ac343ea47d
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-04-13 23:27:13 -0400

    Update test.py

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ def set_seq1(a,b,c,d):
     GPIO.output(b, GPIO.LOW)
     GPIO.output(c, GPIO.LOW)
     GPIO.output(d, GPIO.HIGH)
+    time.sleep(1)
     
 def set_seq2(a,b,c,d):
     
@@ -22,6 +23,7 @@ def set_seq2(a,b,c,d):
     GPIO.output(b, GPIO.LOW)
     GPIO.output(c, GPIO.HIGH)
     GPIO.output(d, GPIO.HIGH)
+    time.sleep(1)
 
 def set_seq3(a,b,c,d):
     
@@ -29,6 +31,7 @@ def set_seq3(a,b,c,d):
     GPIO.output(b, GPIO.HIGH)
     GPIO.output(c, GPIO.HIGH)
     GPIO.output(d, GPIO.LOW)
+    time.sleep(1)
     
 def set_seq4(a,b,c,d):
     
@@ -36,6 +39,7 @@ def set_seq4(a,b,c,d):
     GPIO.output(b, GPIO.HIGH)
     GPIO.output(c, GPIO.LOW)
     GPIO.output(d, GPIO.LOW)
+    time.sleep(1)
 
 def setup_ports(a,b,c,d):
     GPIO.setup(a, GPIO.OUT)
@@ -44,13 +48,11 @@ def setup_ports(a,b,c,d):
     GPIO.setup(d, GPIO.OUT)
 
 try:
-    counter = 0
-    
+    counter = 0   
     
     setup_ports(d1,d2,d3,d4)
-    time.sleep(1)
     
-    while counter <= 50:
+    while counter <= 20:
         set_seq1(d1,d2,d3,d4)
         set_seq2(d1,d2,d3,d4)
         set_seq3(d1,d2,d3,d4)
