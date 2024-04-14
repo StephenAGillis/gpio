b958981721af8057de33b26cf25541c1c590edbe
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-04-14 11:41:42 -0400

    Update py_learn.py

--- py_learn.py
+++ py_learn.py
@@ -1,1 +1,16 @@
-test
+import RPi.GPIO as GPIO
+
+# Set GPIO numbering mode
+GPIO.setmode(GPIO.BCM)
+
+# Set GPIO pin 26 as an input
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Set GPIO pin 26 to LOW
+GPIO.output(5, GPIO.LOW)
+GPIO.output(13, GPIO.LOW)
+GPIO.output(19, GPIO.LOW)
+GPIO.output(26, GPIO.LOW)
