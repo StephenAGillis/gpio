add82dfc663c565b45db0368fee5faababcf90ed
StephenAGillis <160549043+StephenAGillis@users.noreply.github.com>
2024-04-13 20:02:10 -0400

    Update py_learn.py

--- py_learn.py
+++ py_learn.py
@@ -1,15 +1,1 @@
-x = 'spam'
-while x:
-    print(x, end=' ')
-    x = x[1:]
-
-a=0; b=10;
-while a < b:
-    print(a, end=' ')
-    a += 1
-x = 10
-while x:
-    x = x-1
-    if x % 2 != 0: continue
-    print(x, end=' ')
-x=10
+test
